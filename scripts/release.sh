25e8537224dffe125559b32796dc099c4a710a42
muquit <muquit@gmail.com>
2025-03-29 21:36:44 -0400

    update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+########################################################################
+# mk_relase.sh is part of https://github.com/muquit/go-xbuild-go
+# muquit@muquit.com Mar-29-2025 
+########################################################################
+
 mk_release.sh "Please look at [ChangeLog](ChangeLog.md) for details \
 on what has changed in the current version. The binaries are cross-compiled \
 with https://github.com/muquit/go-xbuild-go"

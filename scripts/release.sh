bfcbb8f3bbbcf2e08ac74664517216832ef7e453
muquit <muquit@gmail.com>
2025-03-29 21:35:54 -0400

    add a script to make a release

---
+++ scripts/release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mk_release.sh "Please look at [ChangeLog](ChangeLog.md) for details \
+on what has changed in the current version. The binaries are cross-compiled \
+with https://github.com/muquit/go-xbuild-go"
+
